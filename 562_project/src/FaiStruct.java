ca5f6f45efd6b6fdcc7e3f9ac6ea5057de5e1318
shenyubin1990 <shenyubin1990@gmail.com>
2015-03-25 23:16:36 -0400

    basic mf_struct
    Hello everyone, this version is a basic version to acheive the first
    week requirement. I just want you to know everything is going. I'll
    update it soon.

---
+++ 562_project/src/FaiStruct.java
@@ -0,0 +1,50 @@
+import java.util.List;
+
+
+public class FaiStruct {
+
+	private List<String> projAttri_S;
+	private Integer numGV_N;
+	private List<String> ga_V;
+	private List<String> af_F;
+	private List<String> selectCondition_Q;
+	private List<String> havingCondition_G;
+	public List<String> getProjAttri_S() {
+		return projAttri_S;
+	}
+	public void setProjAttri_S(List<String> projAttri_S) {
+		this.projAttri_S = projAttri_S;
+	}
+	public Integer getNumGV_N() {
+		return numGV_N;
+	}
+	public void setNumGV_N(Integer numGV_N) {
+		this.numGV_N = numGV_N;
+	}
+	public List<String> getGa_V() {
+		return ga_V;
+	}
+	public void setGa_V(List<String> ga_V) {
+		this.ga_V = ga_V;
+	}
+	public List<String> getAf_F() {
+		return af_F;
+	}
+	public void setAf_F(List<String> af_F) {
+		this.af_F = af_F;
+	}
+	public List<String> getSelectCondition_Q() {
+		return selectCondition_Q;
+	}
+	public void setSelectCondition_Q(List<String> selectCondition_Q) {
+		this.selectCondition_Q = selectCondition_Q;
+	}
+	public List<String> getHavingCondition_G() {
+		return havingCondition_G;
+	}
+	public void setHavingCondition_G(List<String> havingCondition_G) {
+		this.havingCondition_G = havingCondition_G;
+	}
+	
+	
+}
