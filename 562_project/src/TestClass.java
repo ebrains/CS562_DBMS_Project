7879fa4bdd4c355d671cd2fdc6cc12f1c2a5abba
conan9079 <wanghan19900709@gmail.com>
2015-04-06 00:48:28 -0400

    Latest version: 04/06/2015
    This is the latest version for our project.
    Now the project is much smarter.  All of the mf struct and algorithms
    are generated from our source code.
    This version can only implement a input for one grouping variable and
    also no "HAVING condition" included in this version.
    The code is a little bit complicated.  Please make sure fully
    understanding the code before we can move to next step.

--- 562_project/src/TestClass.java
+++ 562_project/src/TestClass.java
@@ -1,20 +1,23 @@
 import java.io.*; 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.sql.*;
 import java.util.Arrays;
+
 
 public class TestClass {
 	
 	public static void main(String[] args) {
 		RunJdbc rj = new RunJdbc();
 		rj.generatorCode();
-		rj.printSegment(0, 6);
+		rj.printSegment(1, 8);
 		rj.readInput();
-		rj.connect();
 		rj.printAttributes();
 	    rj.printConstructor();
-	    rj.printSegment(7, 1000);
+	    rj.printSegment(9, 61);
+	    rj.printAlgorithm();
+	    rj.printSegment(67, 1000);
 	}
 	 
 }
@@ -97,7 +100,7 @@ class RunJdbc{
 			FileReader fr = new FileReader(file);
 			BufferedReader bf = new BufferedReader(fr);
 			String line;
-			int count = 0;
+			int count = 1;
 			while((line = bf.readLine()) != null){
 				if (begin <= count && count <= end) 
 					printStr(line);
@@ -110,6 +113,138 @@ class RunJdbc{
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	//Function to generate code for retrieving data from DB
+	public void printAlgorithm(){
+		List<String> projAttributes = Fai.getProjAttri_S();
+		List<String> selectCondition  = Fai.getSelectCondition_Q();
+		List<String> groupingAttri = Fai.getGa_V();
+		List<String> aggreFunction = Fai.getAf_F();
+		
+		//Loop for each grouping variable
+		for(int i = 0; i < Fai.getNumGV_N(); i++){
+			String attriType = null; //Define grouping data attribute type for JDBC connection
+			printStr("        	while(rs.next()) {");
+			printStr("				" + selectCondition.get(i) + " {");
+			//Generate key in HashMap for specified grouping attribute. E.X. "key = rs.getString("cust") + rs.getString("prod");"
+			String key = "";
+			for(int l = 0; l < groupingAttri.size(); l++){
+				if(groupingAttri.get(l).equals("cust") || groupingAttri.get(l).equals("prod")){
+					attriType = "String";
+				}else if(groupingAttri.get(l).equals("state")){
+					attriType = "CharacterStream";
+				}else{
+					attriType = "Int";
+				}
+				if(l == groupingAttri.size() - 1){
+					key += ("rs.get" + attriType + "(\"" + groupingAttri.get(l) + "\")");  //Concatenate the key String for HashMap
+				}else{
+					key += ("rs.get" + attriType + "(\"" + groupingAttri.get(l) + "\") + ");  //Concatenate the key String for HashMap
+				}
+			}
+			printStr("					key = " + key + ";");
+			printStr("					if(map.containsKey(key)){");
+			String aggreResultNew = null; //Assign aggregate result for adding new combination operation
+			List<String> aggreResultNewList = new ArrayList<String>(); //List to store aggreResultNew
+			//Generate code for updating aggregate values
+			for(int k = 0; k < aggreFunction.size(); k++){
+				String[] arr = aggreFunction.get(k).split("_");
+				String aggreResultUpdate = null; //Assign aggregate result for update operation
+				if(Integer.parseInt(arr[2]) == (i + 1)){  //Make sure the aggregate functions are in the current grouping variable
+					switch (arr[0]){
+				 	case "sum":
+				 		aggreResultUpdate = "map.get(key)." + aggreFunction.get(k) + " += rs.getInt(\"" + arr[1] + "\");"; 
+				 		//Parse to E.X. "map.get(key).sum_quant_1 += rs.getInt("quant")";
+				 		aggreResultNew = "fs." + aggreFunction.get(k) + " += rs.getInt(\"" + arr[1] + "\");"; 
+				 		//Parse to E.X. "fs.sum_quant_1 += rs.getInt("quant")";
+				 		aggreResultNewList.add(aggreResultNew);
+				 		break;
+				 	case "cnt":
+				 		aggreResultUpdate = "map.get(key)." + aggreFunction.get(k) + " ++;"; 
+				 		//Parse to E.X. "map.get(key).cnt_quant_1++";
+				 		aggreResultNew = "fs." + aggreFunction.get(k) + " ++;"; 
+				 		//Parse to E.X. "fs.cnt_quant_1++";	
+				 		aggreResultNewList.add(aggreResultNew);
+				 		break;
+				 	case "max":
+				 		aggreResultUpdate = "if (map.get(key)." + aggreFunction.get(k) + " < rs.getInt(\"" + arr[1]+ "\"))\n"
+				 				+ "							map.get(key)." + aggreFunction.get(k) + " = rs.getInt(\"" + arr[1]+ "\");";
+				 		//Parse to E.X. "if (map.get(key).max_quant_1 < rs.getInt("quant"))
+						//	map.get(key).max_quant_1 = rs.getInt("quant")";
+				 		aggreResultNew = "fs." + aggreFunction.get(k) + " = rs.getInt(\"" + arr[1] + "\");"; 
+				 		//fs.max_quant_1 = rs.getInt("quant");
+				 		aggreResultNewList.add(aggreResultNew);
+				 		break;
+				 	case "min":
+				 		aggreResultUpdate = "if (map.get(key)." + aggreFunction.get(k) + " > rs.getInt(\"" + arr[1]+ "\"))\n"
+				 				+ "							map.get(key)." + aggreFunction.get(k) + " = rs.getInt(\"" + arr[1]+ "\");";
+				 		aggreResultNew = "fs." + aggreFunction.get(k) + " = rs.getInt(\"" + arr[1] + "\");";
+				 		aggreResultNewList.add(aggreResultNew);
+				 		break;
+				 	case "avg":
+				 		aggreResultUpdate = "map.get(key)." + aggreFunction.get(k) + " = (int) (map.get(key).sum_" + arr[1] + "_" + arr[2] + "/map.get(key).cnt_" + arr[1] + "_" + arr[2] + ");";  
+				 		//Parse to E.X. "map.get(key).avg_quant_1 = (int) (map.get(key).sum_quant_1/map.get(key).cnt_quant_1)";
+				 		aggreResultNew = "fs." + aggreFunction.get(k) + " = (int) (fs.sum_" + arr[1] + "_" + arr[2] + "/fs.cnt_" + arr[1] + "_" + arr[2] + ");"; 
+				 		//Parse to E.X. "fs.avg_quant_1 = (int) (fs.sum_quant_1/fs.cnt_quant_1)";
+				 		aggreResultNewList.add(aggreResultNew);
+				 		break;
+					}	
+				}
+				printStr("						" + aggreResultUpdate);
+			}
+			//Generate code for adding new combination of grouping attributes
+			printStr("					}else {");
+			printStr("						FaiStruct fs = new FaiStruct();");
+			//Generate code for assigning attribute values
+			for (int l = 0; l < groupingAttri.size(); l++) {
+				printStr("						fs." + groupingAttri.get(l) + " = rs.get"
+						+ attriType + "(\"" + groupingAttri.get(l) + "\");");
+				// Parse to E.X. "fs.cust = rs.getString("cust");"
+			}
+			//Generate code for assigning aggregate values
+			for(int m = 0; m < aggreResultNewList.size(); m++){
+				printStr("						" + aggreResultNewList.get(m));
+				//Pare to E.X. "fs.sum_quant_1 += rs.getInt("quant");"
+			}
+			
+			printStr("						map.put(key, fs);\n"
+					+ "					}\n" 
+					+ "				}\n" 
+					+ "			}\n");
+		}
+		
+		//Generate code for printing out projected attributes and aggregate functions
+		for(int i = 0; i < projAttributes.size(); i++){
+			if(i == projAttributes.size() - 1){
+				printStr("			System.out.printf(\"%-7s  \\n\", \"" + projAttributes.get(i) + "\");");
+			}else{
+				printStr("			System.out.printf(\"%-7s  \", \"" + projAttributes.get(i) + "\");"); 
+			}
+		}
+		//Generate code for printing out showing results statements
+		printStr("			Iterator<String> iter = map.keySet().iterator();");
+		printStr("			while(iter.hasNext()){");
+		printStr("				FaiStruct fs = map.get(iter.next());");
+		//Generate code for printing out showing grouping attributes statements
+		for(int i = 0; i < groupingAttri.size(); i++){
+			printStr("				System.out.printf(\"%-7s  \", fs." + groupingAttri.get(i) + ");"); //System.out.printf("%-7s  ", fs.cust);
+		}
+		//Generate code for printing out showing aggregate functions statements
+		for(int i = 0; i < aggreFunction.size(); i++){
+			if(i ==  aggreFunction.size() - 2 && !projAttributes.contains(aggreFunction.get(i+1))){
+				printStr("				System.out.printf(\"%11s  \\n\", fs." + aggreFunction.get(i) + ");");
+				break;
+			}
+			if(projAttributes.contains(aggreFunction.get(i))){
+				if(i == aggreFunction.size() - 1){
+					printStr("				System.out.printf(\"%11s  \\n\", fs." + aggreFunction.get(i) + ");");
+				}else{
+					printStr("				System.out.printf(\"%11s  \", fs." + aggreFunction.get(i) + ");"); //System.out.printf("%-7s  ", fs.avg_quant_1);
+				}
+	 		}
+		}
+		printStr("			}");
 	}
 	
 	//Function to read input file and initialize FaiStruct
@@ -144,6 +279,16 @@ class RunJdbc{
 			Fai.setAf_F(attribute_set);
 			System.out.println(Fai.getAf_F());
 			
+			arr = lineList.get(9).split(", ");
+			attribute_set = Arrays.asList(arr);
+			Fai.setSelectCondition_Q(attribute_set);
+			System.out.println(Fai.getSelectCondition_Q());
+			
+			arr = lineList.get(11).split(", ");
+			attribute_set = Arrays.asList(arr);
+			Fai.setHavingCondition_G(attribute_set);
+			System.out.println(Fai.getHavingCondition_G());
+			
 			bf.close();
 			fr.close();
 		} catch (FileNotFoundException e) {
@@ -152,17 +297,6 @@ class RunJdbc{
 			e.printStackTrace();
 		}
 	}
-	
-    //Function to connect to the database
-	public void connect(){
-		 try {
-	        Class.forName("org.postgresql.Driver");     //Loads the required driver
-	        System.out.println("Success loading Driver!");
-	     } catch(Exception exception) {
-	        System.out.println("Fail loading Driver!");
-	        exception.printStackTrace();
-	     }
-	 }
 	
 	//Function to generate the code for run query
 	public void generatorCode(){
@@ -181,7 +315,7 @@ class RunJdbc{
 		 }
 	 } 
 	
-	 //Function to print the String 
+	//Function to print the String 
 	public void printStr(String content){
 		 //File file = new File("D:/generator_code.java");
 		 try{
@@ -207,7 +341,7 @@ class RunJdbc{
 	 		return "		char ";
 	 	default:
 	 		return "		int ";
-		}			
+		}	
 	 }
 	
 	//Function to retrieve data type from database
@@ -224,11 +358,9 @@ class RunJdbc{
 	        	for (String attribute : attribute_set) {
 	        		if (rs.getString("column_name").equals(attribute)){
 	        			list.add(judge(rs.getString("data_type")) + attribute + ";");
-		        		//print_str(judge(rs.getString("data_type")) + attribute + ";");
 		        		con_sen.add(attribute + (judge(rs.getString("data_type")) == "int"?" = 0":" = null") + ";");
 		        	}
 	        	}
-        		//System.out.printf("%s ",rs.getString(4)); 
 	        }
 		 } catch(SQLException e) {
 	         System.out.println("Connection URL or username or password errors!");
